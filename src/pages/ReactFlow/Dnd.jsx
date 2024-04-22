d2a355dc7f6e59dff0f550cc5e1f01e57654c1a0
Vinay Kumar <92167129+vina-y-kumar@users.noreply.github.com>
2024-04-22 17:33:25 +0530

    flowgraph update

---
+++ src/pages/ReactFlow/Dnd.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useRef, useCallback } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  MiniMap,
+  Background,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+
+import Sidebar from './Sidebar';
+
+import './dnd.css';
+import initialNodes from "./nodes.jsx";
+import initialEdges from "./edges.jsx";
+import e from 'cors';
+
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const DnDFlow = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
+  const[editValue,setEditValue]=useState(nodes.data);
+  const[id,setId]=useState();
+
+  const onNodeClick=(e,val)=>{
+    setEditValue(val.data.label);
+    setId(val.id);
+  }
+  const handleChange=(e)=>{
+    e.preventDefault();
+    setEditValue(e.target.value);
+  }
+  const handleEdit=()=>{
+    const res=nodes.map((item)=>{
+      if(item.id===id){
+        item.data={
+          ...item.data,
+          label:editValue
+        }
+      }
+      return item;
+    })
+    setNodes(res);
+    setEditValue('');
+  }
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance],
+  );
+
+  return (
+    <div className="dndflow">
+      <div className="updatenode">
+        
+        <input type="text" placeholder='Update Nodes' value={editValue} onChange={handleChange}/><br/>
+        <button style={{marginLeft:"35px"}} className='btn btn-primary' onClick={handleEdit} >Update:</button>
+      </div>
+      <ReactFlowProvider>
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodeClick={(e,val)=>onNodeClick(e,val)}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Controls />
+            <MiniMap/>
+            <Background/>
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </ReactFlowProvider>
+    </div>
+  );
+};
+
+export default DnDFlow;
---
+++ src/pages/ReactFlow/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <aside>
+      <h2>Add Nodes</h2>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+    </aside>
+  );
+};
